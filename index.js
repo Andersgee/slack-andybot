1c519ddd22b1a622d378fe75dd781f9f80d29890
Anders Gustafsson <andersgee@gmail.com>
2021-07-20 19:26:49 +0200

    post /slack-andybot-event with json responds

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html)"));
 });
@@ -12,3 +11,19 @@ app.get("/", (req, res) => {
 app.get("/stuff", (req, res) => {
   res.json({ someKey: "someValue" });
 });
+
+app.get("/otherstuff", (req, res) => {
+  res.send("simple sting");
+});
+
+app.use(express.json());
+app.post("/slack-andybot-event", (req, res) => {
+  const obj = req.body;
+  //console.log(obj);
+  const str = obj.challenge || "nope..";
+  res.send(str);
+  //json would work aswell: https://api.slack.com/events/url_verification
+  //res.json();
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
