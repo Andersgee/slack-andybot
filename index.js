3141aafc1231ecbb0ed0d869b385f9236c6e3162
Anders Gustafsson <andersgee@gmail.com>
2021-07-20 21:58:35 +0200

    correct post request (with node-fetch)

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fetch = require("node-fetch");
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+async function postMessage(msg) {
+  const body = {
+    channel: "webassemblytext",
+    text: msg,
+  };
+
+  const res = await fetch("https://slack.com/api/chat.postMessage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf8",
+      Authorization: `Bearer ${BOT_TOKEN}`,
+    },
+    body: JSON.stringify(body),
+  });
+  //const resdata = await res.json();
+  //console.log("resdata: ", resdata);
+}
 
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
@@ -23,7 +44,8 @@ app.post("/slack-andybot-event", (req, res) => {
   res.sendStatus(200);
 
   if (payload.event.type === "app_mention") {
-    const text = payload.event.text;
+    //const text = payload.event.text;
+    postMessage("basic response to app_mention");
     //make a post request to slack here with what
     //see: https://api.slack.com/methods/chat.postMessage
   }
