dbad5d573aa1be69fdf8ca80dd2b2942ac19348d
Anders Gustafsson <andersgee@gmail.com>
2021-07-28 12:54:42 +0200

    post instead of get

--- main.js
+++ main.js
@@ -5,14 +5,28 @@ import { postMessage } from "./src/messaging.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
+/*
 function handleWebassemblytextAction() {
   const channel = "webassemblytext";
   const text = "triggered by simple get request";
   postMessage(channel, text);
 }
+*/
+function handleWebassemblytextAction(req, res) {
+  const payload = req.body;
+  res.sendStatus(200);
+
+  const channel = "webassemblytext";
+  if (payload.supersafepassword === "abcda") {
+    postMessage(channel, "Triggered with correct password");
+  } else {
+    postMessage(channel, "Triggered WITHOUT correct password");
+  }
+}
 
 app.use(express.json());
 app.post("/slack-andybot-event", handleEvent);
-app.get("/webassemblytext-action", handleWebassemblytextAction);
+
+//https://slack-andybot.herokuapp.com/webassemblytext-action
+app.post("/webassemblytext-action", handleWebassemblytextAction);
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
