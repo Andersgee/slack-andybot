d8fd86ab96648e8f6fe94f079f3c59638f61a09a
Anders Gustafsson <andersgee@gmail.com>
2021-07-28 13:05:15 +0200

    Ok, lets write a more informative message now that it only triggers with correct password from github action

--- main.js
+++ main.js
@@ -5,22 +5,17 @@ import { postMessage } from "./src/messaging.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-/*
-function handleWebassemblytextAction() {
-  const channel = "webassemblytext";
-  const text = "triggered by simple get request";
-  postMessage(channel, text);
-}
-*/
+
 function handleWebassemblytextAction(req, res) {
   const payload = req.body;
   res.sendStatus(200);
 
   const channel = "webassemblytext";
   if (payload.supersafepassword === "abcda") {
-    postMessage(channel, "Triggered with correct password");
-  } else {
-    postMessage(channel, "Triggered WITHOUT correct password");
+    postMessage(
+      channel,
+      "Info: Andersgee/WebAssemblyText.jl was just updated."
+    );
   }
 }
 
